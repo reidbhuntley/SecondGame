92d6ebab50f8d2dfa70b2b3fc351dd74ce8df5e8
reidbhuntley <reidbhuntley@gmail.com>
2018-01-19 17:05:40 -0500

    quick bugfix

--- SecondGame/main.cpp
+++ SecondGame/main.cpp
@@ -14,6 +14,27 @@ void processInput(GLFWwindow *window) {
 		glfwSetWindowShouldClose(window, true);
 }
 
+GLFWwindow* createContext() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		exit(-1);
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		exit(-1);
+	}
+	return window;
+}
+
 int main()
 {
 	GLFWwindow* window = createContext();
@@ -39,25 +60,4 @@ int main()
 
 	glfwTerminate();
 	return 0;
-}
-
-GLFWwindow* createContext() {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		exit(-1);
-	}
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		exit(-1);
-	}
-	return window;
 }
